0ef5d391f5c14be63e45cbf515371593040b5347
Sophia <spuertas@micds.org>
2018-06-21 14:02:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+function getUKTime() {
+  new Date().getHours() + 6
+}
+ 
+getUKTime()
+
+function discountTicket(originalPrice) {
+  let price = origialPrice*.8;
+}
+discountTicket()
